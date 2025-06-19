27a4d55fba12c1facaf6bdd202a3eb186a0666d8
Kamit6337 <kamit6337@gmail.com>
2025-06-19 11:01:44 +0530

    add mobile number validation in address (create and update)

--- controllers/address/createNewAddress.js
+++ controllers/address/createNewAddress.js
@@ -1,12 +1,21 @@
 import createNewAddressDB from "../../database/Address/createNewAddressDB.js";
 import catchAsyncError from "../../lib/catchAsyncError.js";
 import HandleGlobalError from "../../lib/HandleGlobalError.js";
+import { parsePhoneNumberFromString } from "libphonenumber-js";
 
 const createNewAddress = catchAsyncError(async (req, res, next) => {
   const userId = req.userId;
 
-  const { name, dial_code, mobile, address, country, state, district } =
-    req.body;
+  const {
+    name,
+    dial_code,
+    mobile,
+    address,
+    country,
+    state,
+    district,
+    country_code,
+  } = req.body;
 
   if (
     !name ||
@@ -15,9 +24,21 @@ const createNewAddress = catchAsyncError(async (req, res, next) => {
     !address ||
     !country ||
     !state ||
-    !district
+    !district ||
+    !country_code
   ) {
     return next(new HandleGlobalError("All field must provide", 404));
+  }
+
+  const mobileNumWithDialCode = dial_code + mobile;
+
+  const phoneNumber = parsePhoneNumberFromString(
+    mobileNumWithDialCode,
+    country_code
+  );
+
+  if (!phoneNumber || !phoneNumber.isValid()) {
+    return next(new HandleGlobalError("Invalid Mobile Number", 404));
   }
 
   const obj = {
--- controllers/address/updateAddress.js
+++ controllers/address/updateAddress.js
@@ -1,12 +1,22 @@
 import updateAddressDB from "../../database/Address/updateAddressDB.js";
 import catchAsyncError from "../../lib/catchAsyncError.js";
 import HandleGlobalError from "../../lib/HandleGlobalError.js";
+import { parsePhoneNumberFromString } from "libphonenumber-js";
 
 const updateAddress = catchAsyncError(async (req, res, next) => {
   const userId = req.userId;
 
-  const { _id, name, dial_code, mobile, address, country, state, district } =
-    req.body;
+  const {
+    _id,
+    name,
+    dial_code,
+    mobile,
+    address,
+    country,
+    state,
+    district,
+    country_code,
+  } = req.body;
 
   if (
     !_id ||
@@ -16,9 +26,21 @@ const updateAddress = catchAsyncError(async (req, res, next) => {
     !address ||
     !country ||
     !state ||
-    !district
+    !district ||
+    !country_code
   ) {
     return next(new HandleGlobalError("All field must provide", 404));
+  }
+
+  const mobileNumWithDialCode = dial_code + mobile;
+
+  const phoneNumber = parsePhoneNumberFromString(
+    mobileNumWithDialCode,
+    country_code
+  );
+
+  if (!phoneNumber || !phoneNumber.isValid()) {
+    return next(new HandleGlobalError("Invalid Mobile Number", 404));
   }
 
   const obj = {
