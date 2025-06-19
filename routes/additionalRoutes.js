27926958194c25aaf458c50151802fe46f9217b9
Kamit6337 <kamit6337@gmail.com>
2025-06-19 12:51:46 +0530

    update routes

--- routes/additionalRoutes.js
+++ routes/additionalRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import getCountryStates from "../controllers/location/getCountryStates.js";
 import getStateCities from "../controllers/location/getStateCities.js";
-import getCountryFromLanLon from "../controllers/additional/getCountryFromLanLon.js";
 import getAllCountries from "../controllers/additional/getAllCountries.js";
 import getCurrencyExchange from "../controllers/additional/getCurrencyExchange.js";
 
 const router = express.Router();
 
 router.get("/countries", getAllCountries);
-router.get("/country", getCountryFromLanLon);
 
 router.get("/states", getCountryStates);
 router.get("/cities", getStateCities);
